990853a2b134e2f08e021221c520dd0fca311529
taus232323 <tayc13@list.ru>
2023-02-05 18:50:41 +0300

    complete one more exercise

---
+++ 05_zoo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# есть список животных в зоопарке
+
+zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
+
+# посадите медведя (bear) между львом и кенгуру
+#  и выведите список на консоль
+
+zoo.insert(1, 'bear')
+
+# добавьте птиц из списка birds в последние клетки зоопарка
+birds = ['rooster', 'ostrich', 'lark', ]
+
+zoo.extend(birds)
+
+#  и выведите список на консоль
+
+
+# уберите слона
+#  и выведите список на консоль
+
+zoo.pop(3)
+for animals in zoo:
+    print(animals)
+
+# выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
+# Номера при выводе должны быть понятны простому человеку, не программисту.
+
+print('Lion is in cell number ', zoo.index('lion'))
+print('Lark is in cell number ', zoo.index('lark'))
+
+
+
+
+
